c4500bd90c2f8b65cee4ab1651e86f9a56d4ed4a
Ariel Moatti <github@arielmoatti.com>
2020-09-10 17:52:09 +0200

    "submitting array exercise"

---
+++ week01/array-exercise.js
@@ -0,0 +1,49 @@
+//1. exercise
+function each(candidate, callback) {
+    if (Array.isArray(candidate)) {
+        for (var i = 0; i < candidate.length; i++) {
+            callback(candidate[i], i);
+        }
+    } else if (typeof candidate == "object") {
+        for (var key in candidate) {
+            callback(candidate[key], key);
+        }
+    }
+}
+
+each(
+    {
+        a: 1,
+        b: 2,
+    },
+    function (val, name) {
+        console.log("The value of " + name + " is " + val);
+    }
+);
+
+each(["a", "b"], function (val, idx) {
+    console.log("The value of item " + idx + " is " + val);
+});
+
+//2. exercise
+function revOrder(arr) {
+    var revArr = arr;
+    return revArr.reverse();
+}
+
+var result2 = revOrder([1, 2, 3, 4]);
+console.log("result2", result2);
+
+//3. exercise
+function getLessThanZero(arr) {
+    var resultArr = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] < 0) {
+            resultArr.push(arr[i]);
+        }
+    }
+    return resultArr;
+}
+
+var result3 = getLessThanZero([1, 2, -1, 0, 12]);
+console.log("result3", result3);
