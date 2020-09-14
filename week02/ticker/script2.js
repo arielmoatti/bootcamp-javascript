57eb26ef4fc79d8102572af253ed24070edb3f35
Ariel Moatti <github@arielmoatti.com>
2020-09-14 19:00:01 +0200

    submitting tickers exercise

--- week02/ticker/script2.js
+++ week02/ticker/script2.js
@@ -1,20 +1,19 @@
-(function () {
-    var headlineBox = document.getElementById("headlines");
-    // console.log("headlineBox", headlineBox);
-    var left = headlineBox.offsetLeft;
-    // console.log("left", left);
-    var links = document.getElementsByTagName("a");
-    // console.log("links", links);
+var headlineBox = document.getElementById("headlines");
+// console.log("headlineBox", headlineBox);
+var left = headlineBox.offsetLeft;
+// console.log("left", left);
+var links = document.getElementsByTagName("a");
+// console.log("links", links);
 
-    function moveTickers() {
-        left--;
-        headlineBox.style.left = left + "px";
-        if (left <= -links[0].offsetWidth) {
-            left += links[0].offsetWidth;
-            links[0].appendChild(links[0]);
-        }
-
-        requestAnimationFrame(moveTickers);
+function moveTickers() {
+    left--;
+    headlineBox.style.left = left + "px";
+    if (left <= -links[0].offsetWidth) {
+        left += links[0].offsetWidth;
+        headlineBox.appendChild(links[0]);
     }
-    console.log(left);
-})();
+
+    requestAnimationFrame(moveTickers);
+}
+
+moveTickers();
