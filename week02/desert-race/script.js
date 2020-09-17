9578d942a7de5496839a98e03fce6d244857c3f6
arielmoatti <github@arielmoatti.com>
2020-09-18 00:35:08 +0200

    pc sync

--- week02/desert-race/script.js
+++ week02/desert-race/script.js
@@ -1,42 +1,44 @@
-var runners = document.getElementsByClassName("runner");
-var donuts = document.getElementsByClassName("donut");
+(function () {
+    var runners = document.getElementsByClassName("runner");
+    var donuts = document.getElementsByClassName("donut");
 
-var ghost = 0;
-var horse = 0;
-var alien = 0;
-var robot = 0;
+    var ghost = 0;
+    var horse = 0;
+    var alien = 0;
+    var robot = 0;
 
-function getRandomNumber() {
-    return Math.floor(Math.random() * 101);
-}
+    function getRandomNumber() {
+        return Math.floor(Math.random() * 101);
+    }
 
-function winnerCheck() {
-    for (var i = 0; i < runners.length; i++) {
-        // console.log(runners[i]);
-        if (
-            runners[i].offsetLeft + runners[i].offsetWidth >=
-            donuts[i].offsetLeft
-        ) {
-            console.log("game over!", runners[i].innerText);
-            document.removeEventListener("keydown", game);
+    function winnerCheck() {
+        for (var i = 0; i < runners.length; i++) {
+            // console.log(runners[i]);
+            if (
+                runners[i].offsetLeft + runners[i].offsetWidth >=
+                donuts[i].offsetLeft
+            ) {
+                console.log("game over!", runners[i].innerText);
+                document.removeEventListener("keydown", game);
+            }
         }
     }
-}
 
-function game(event) {
-    if (event.keyCode === 32) {
-        ghost += getRandomNumber();
-        horse += getRandomNumber();
-        alien += getRandomNumber();
-        robot += getRandomNumber();
+    function game(event) {
+        if (event.keyCode === 32) {
+            ghost += getRandomNumber();
+            horse += getRandomNumber();
+            alien += getRandomNumber();
+            robot += getRandomNumber();
 
-        runners[0].style.left = ghost + "px";
-        runners[1].style.left = horse + "px";
-        runners[2].style.left = alien + "px";
-        runners[3].style.left = robot + "px";
+            runners[0].style.left = ghost + "px";
+            runners[1].style.left = horse + "px";
+            runners[2].style.left = alien + "px";
+            runners[3].style.left = robot + "px";
 
-        winnerCheck();
+            winnerCheck();
+        }
     }
-}
 
-document.addEventListener("keydown", game);
+    document.addEventListener("keydown", game);
+})();
