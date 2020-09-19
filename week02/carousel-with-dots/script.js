379ef9d03a604753a82ecff7378721ec2f9f8b38
arielmoatti <github@arielmoatti.com>
2020-09-19 21:35:53 +0200

    finalizing carousel with extra navigation features

--- week02/carousel-with-dots/script.js
+++ week02/carousel-with-dots/script.js
@@ -1,68 +1,122 @@
 (function () {
     var kitty = document.querySelectorAll(".kitty-container img"); //grab all images into an object
     var dots = document.getElementsByClassName("dot"); //grabs all the dots into an object
-    // console.log("dots", dots);
+    var dotsContainer = document.querySelector(".dots"); //grabs the container of the dots for effects
     var cur = 0; // set counter for index
-    var timer = setTimeout(moveKitty(), 5000); //invoke function after 3 sec. and stores the return in a var
+    var isAnimating; //will be boolean to detect trasition process
+    var timer = setTimeout(moveKitty, 3000); //invoke function after ... sec. and stores the return in a var
 
     //----------- event listener to dots click
     for (var i = 0; i < dots.length; i++) {
+        //loop through the dots to detect which dot was clicked
         dots[i].addEventListener("click", function (e) {
-            clearTimeout(timer);
+            // adds eL to the individual dot
+            if (isAnimating) {
+                //checks if transition is in progress
+                return; //to ignore clicks while transitioning
+            }
             for (var i = 0; i < dots.length; i++) {
                 if (dots[i] == e.target) {
-                    setTimeout(moveKitty(i), 5000);
-                    console.log("index after click=", i);
-                    break;
+                    if (i == cur) {
+                        return;
+                    }
+                    clearTimeout(timer);
+                    moveKitty(i); //passed the index to the main function as argument
                 }
             }
         });
     }
 
+    //--------------------------sets cursor to not allow during  hover over
+    // the same same image
+    for (var i = 0; i < dots.length; i++) {
+        dots[i].addEventListener("mouseover", function (e) {
+            // if (isAnimating) {
+            //     return;
+            // } else {
+            for (var i = 0; i < dots.length; i++) {
+                if (dots[i] == e.target) {
+                    if (i == cur) {
+                        dots[cur].classList.add("cursorNot");
+                        return;
+                    }
+                    // else {
+                    //     dots[cur].classList.remove("cursorNot");
+                    // }
+                }
+            }
+            // }
+        });
+    }
+
+    //--------------- resets the cursor state upon leaving the hover
+    for (var i = 0; i < dots.length; i++) {
+        dots[i].addEventListener("mouseleave", function (e) {
+            // if (isAnimating) {
+            //     return;
+            // } else {
+            for (var i = 0; i < dots.length; i++) {
+                if (dots[i] == e.target) {
+                    if (i == cur) {
+                        dots[cur].classList.remove("cursorNot");
+                        return;
+                    }
+                }
+            }
+            // }
+        });
+    }
+    //-----------------main carousel function
     function moveKitty(arg) {
-        console.log("index value was: ", cur);
+        // console.log("arg initial status", arg);
+        isAnimating = true;
+
+        // console.log("index value was: ", cur);
+        kitty[cur].classList.remove("onscreen");
+        kitty[cur].classList.add("offscreen-left");
+        dots[cur].classList.remove("on");
         //only runs when
         if (typeof arg != "undefined") {
-            arg = cur;
+            cur = arg;
         } else {
             // remove current item from screen and push left
-            kitty[cur].classList.remove("onscreen");
-            dots[cur].classList.remove("on");
-            kitty[cur].classList.add("offscreen-left");
             cur++; //incremate for the coming image
             //check when reached the max number of images
             if (cur === kitty.length) {
                 cur = 0; //reset index back to 0
             }
-            // pull the next item into the screen
-            kitty[cur].classList.add("onscreen"); //cur is already 1 higher
-            dots[cur].classList.add("on");
         }
+        // pull the next item into the screen
+        kitty[cur].classList.add("onscreen"); //notice cur is already 1 higher
     }
 
-    // puts the current item to the queue
+    //------------------------- dim and not allow interaction with navigation
+    //dots during transition
+    document.addEventListener("transitionstart", function () {
+        dotsContainer.classList.remove("cursorNot");
+        dots[cur].classList.remove("on");
+        dotsContainer.classList.add("dotOff");
+        dotsContainer.classList.add("cursorNot");
+    });
+
+    //--------------------- puts the current item to the queue
     document.addEventListener("transitionend", function (e) {
+        //sets isAnimating to false, as it is the transition's end
+        isAnimating = false;
+        dots[cur].classList.add("on");
+        dotsContainer.classList.remove("dotOff");
+        dotsContainer.classList.remove("cursorNot");
+        //cycle through indexes to reset cursor state
+        for (var i = 0; i < dots.length; i++) {
+            dots[i].classList.remove("cursorNot");
+        }
         //check only transition for this class
         if (e.target.classList.contains("offscreen-left")) {
-            console.log("it is now transitionend of offscreen-left");
+            // console.log("it is now transitionend of offscreen-left");
             e.target.classList.remove("offscreen-left"); //removes class and reset status
-        } else {
-            return; //ignores
+            timer = setTimeout(moveKitty, 3000); //run again, infinite...
         }
-        setTimeout(moveKitty(), 5000); //run again, infinite...
     });
+
+    //
 })();
-
-//check if cycle has completed
-//can change from 3 to the array length
-
-// moveKitty();
-//****remove offscreen-left only after it finished the transition
-//document.addEventListener("transitionend", function)
-/// but to target only the transition to "offscreen-left"
-//need to determine if the transition that ended was "onscreen" do nothing,
-//else - remove the class offscreen-left
-//--------check how to target specific transition for "transitionend"
-//console log the event object [function (event)]
-
-//***********if kitty[] == 3 then invoke function
