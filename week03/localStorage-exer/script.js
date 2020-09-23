0476628706d0df7f55394327322fce8af7de36eb
arielmoatti <github@arielmoatti.com>
2020-09-23 19:58:48 +0200

    submit localStorage with error handler

--- week03/localStorage-exer/script.js
+++ week03/localStorage-exer/script.js
@@ -1,10 +1,44 @@
 var text = $("textarea");
+placeholderText = "type your secret message here...";
+text[0].placeholder = placeholderText;
 
-$(".save").click(function () {
-    localStorage.setItem("storedTextarea", text.val());
-    text.val("your text has been saved");
-});
+try {
+    $(".save").click(function () {
+        localStorage.setItem("storedTextarea", text.val());
+        text.css({
+            // "background-color": "firebrick",
+            color: "darkmagenta",
+        });
+        text.val(
+            "your message was released into the ocean... \nclose your browser and reload this page to retrieve your message later!"
+        );
+    });
 
-$(".retrieve").click(function () {
-    text.val(localStorage.getItem("storedTextarea"));
-});
+    $(".retrieve").click(function () {
+        text.css({
+            // "background-color": "firebrick",
+            color: "mediumblue",
+        });
+        text.val(
+            "ohh... the message reads: " +
+                '"' +
+                localStorage.getItem("storedTextarea") +
+                '"'
+        );
+    });
+
+    $(".clear").click(function () {
+        text.val("");
+        text[0].placeholder = placeholderText;
+        text.css({
+            "background-color": "inherit",
+            color: "inherit",
+        });
+    });
+
+    text.focus(function () {
+        text.prop("placeholder", "");
+    });
+} catch (err) {
+    console.log("this browser is stubborn", err);
+}
