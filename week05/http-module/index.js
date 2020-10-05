b6b94d517e7920f1e4b2f163754520e021d5a281
Ariel Moatti <github@arielmoatti.com>
2020-10-05 18:20:30 +0200

    submit http-module 2 exercises

--- week05/http-module/index.js
+++ week05/http-module/index.js
@@ -15,20 +15,20 @@ const server = http.createServer((request, response) => {
     console.log("request url: ", request.url);
     console.log("request headers: ", request.headers);
 
-    //part 2.
-    let infoToAppend;
+    /////////////////part 2. starts here
     let currentdate = new Date();
     let datetime = `${currentdate.getDate()}.${
         currentdate.getMonth() + 1
     }.${currentdate.getFullYear()} ${currentdate.getHours()}:${currentdate.getMinutes()}:${currentdate.getSeconds()}
     `;
-    infoToAppend = `${datetime}, ${request.method}, ${request.url}`;
-
+    let infoToAppend = `\n${datetime}, ${request.method}, ${request.url}, ${request.headers["user-agent"]}`;
+    //.
     fs.appendFile("requests.txt", infoToAppend, (err) => {
         if (err) {
             console.log("error in append file!");
         }
     });
+    //////////////part 2. ends here
     if (request.method === "GET" || request.method === "HEAD") {
         response.setHeader("content-type", "text-html");
         response.statusCode = 200;
@@ -57,21 +57,3 @@ const server = http.createServer((request, response) => {
 }); //closes the createServer method for const server
 
 server.listen(8080, () => console.log("server is listening...")); //terminal feedback
-
-// 05.10.2020
-//http requests
-
-//If the request method is GET or HEAD, set the content type of the response to 'text/html' and the status code to 200.
-//for GET we send a text, not for HEAD
-
-//
-//If the request method is POST, log the request body to the console
-//we did this
-
-//Part 2
-//For each request made to your server
-//each one!!!
-//use fs.appendFile to add to a file named "requests.txt" a line with the following information in it:
-// async function!
-//need to read documentation, what arguments, what order
-//this runs for every singele reuest. every time we make request it has all the info about the reuqest.  Use /n for a new line
