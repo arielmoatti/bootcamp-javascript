319f3380513e3fd0836e6b06822c64f3a1e18df0
Ariel Moatti <github@arielmoatti.com>
2020-10-13 18:18:57 +0200

    struggling with fs promises

---
+++ week06/fun-with-fs-promises/index.js
@@ -0,0 +1,44 @@
+///////////////////////
+/////fun with fs //////
+///////////////////////
+
+const { readdir, stat } = require("fs").promises;
+const myPath = `${__dirname}/files`;
+
+// const fs = require("fs");
+
+function logSizes(dir) {
+    return readdir(dir).then((content) => {
+        for (let i = 0; i < content.length; i++) {
+            if (content[i].isFile()) {
+                //if it's a file do this
+                stat(`${dir}/${content[i].name}`).then((data) => {
+                    console.log(`${dir}/${content[i].name}:`, data["size"]);
+                }); //closes the stat promise
+            } else if (content[i].isDirectory()) {
+                //if it's a directory do this
+                logSizes(`${dir}/${content[i].name}`); // call the function (recursive)
+            } //closes the if statement checking for directory=true
+        } // closes the for loop on all files
+    }); //closes the readdir promise
+} //closes the logSized function
+
+logSizes(myPath);
+
+/*
+
+function logSizes(dir) {
+    return readdir(dir).then((content) => {
+        for (let i = 0; i < content.length; i++) {
+            if (content[i].isFile) {
+                stat(`${dir}/${content[i].name}`).then((data) => {
+                    console.log(`${dir}/${content[i].name}:`, data["size"]);
+                });
+            } else if (content[i].isDirectory) {
+                logSizes(`${dir}/${content[i].name}`);
+            }
+        }
+    });
+}
+console.log(logSizes(myPath));
+*/
