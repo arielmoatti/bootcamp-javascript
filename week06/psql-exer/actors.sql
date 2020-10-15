c4d1c90d8d152a58966172b2cf4ea9ff6a7cc95f
Ariel Moatti <github@arielmoatti.com>
2020-10-15 12:38:29 +0200

    submit psql 1st exercise update

--- week06/psql-exer/actors.sql
+++ week06/psql-exer/actors.sql
@@ -4,19 +4,19 @@ CREATE TABLE actors (
     id SERIAL primary key,
     Name VARCHAR(127) NOT NULL,
     Age INT,
-    Oscars INT
+    "Number of Oscars" INT
 );
 
--- SELECT name FROM actors WHERE oscars > 1;
+-- SELECT name FROM actors WHERE "Number of oscars" > 1;
 -- SELECT name FROM actors WHERE age > 30;
 
-INSERT INTO actors (Name, Age, Oscars)
+INSERT INTO actors (Name, Age, "Number of Oscars")
 VALUES ('Leonardo DiCaprio', 41, 1);
-INSERT INTO actors (Name, Age, Oscars)
+INSERT INTO actors (Name, Age, "Number of Oscars")
 VALUES ('Jennifer Lawrence', 25, 1);
-INSERT INTO actors (Name, Age, Oscars)
+INSERT INTO actors (Name, Age, "Number of Oscars")
 VALUES ('Samuel L. Jackson', 67, 0);
-INSERT INTO actors (Name, Age, Oscars)
+INSERT INTO actors (Name, Age, "Number of Oscars")
 VALUES ('Meryl Streep', 66, 3);
-INSERT INTO actors (Name, Age, Oscars)
+INSERT INTO actors (Name, Age, "Number of Oscars")
 VALUES ('John Cho', 43, 0);
